8144f8d3d10f5021ea278e2156d5135656456051
donalddop <donald.nikkessen@gmail.com>
2019-12-12 10:53:26 +0100

    Perceptual model completed

--- model1.py
+++ model1.py
@@ -7,6 +7,8 @@ from skimage import feature
 from scipy.ndimage import gaussian_filter
 from scipy import io
 from sklearn.naive_bayes import GaussianNB
+from PIL import Image
+from itertools import combinations
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -31,40 +33,51 @@ def loadmat(matfile):
     else:
         return {name: np.transpose(f.get(name)) for name in f.keys()}
 
-def edge_detect(train_set, train_iter, sigma):
+def perceptual_model(dataset, folder, sigma):
     num = 0 # For naming output files
+    edges = [] # storing edge detected images
+    # Load training data using dataloader
+    loader = torch.utils.data.DataLoader(dataset, batch_size=32,
+                                                num_workers=4, shuffle=True)
 
     # Iterate over all batches
-    for images, labels in train_iter:
+    for images, labels in loader:
         # Select an image for edge detection
         for j in range(len(images)):
             # Convert image to numpy array
             im = np.transpose(images[j].numpy(), (1, 2, 0))
-
             # Convert rgb to grayscale for edge detection
             imgray = np.dot(im[...,:3], [0.299, 0.587, 0.114])
             # Compute the Canny filter for two values of sigma
-            edges = feature.canny(imgray, sigma)#, high_threshold=12)
+            edges.append(feature.canny(imgray, sigma))#, high_threshold=12)
 
             # Apply gaussian filter
             # edges = gaussian_filter(edges, sigma=1)
 
             # Save the generated image
-            filename = 'Edges/' + train_set + '/' + str(num) +'.jpg'
-            plt.imsave(filename, edges, cmap=cm.gray)
+            filename = 'Edges/' + folder + '/' + str(num) +'.jpg'
+            plt.imsave(filename, edges[num], cmap=cm.gray)
             num += 1
-    #edges = gaussian_filter(edges, sigma=.5)
-    plt.imshow(edges, cmap=cm.gray)
-    plt.show()
 
-def GNB():
+    # Create the perceptual RDM using overlapping pixel counts
+    rdm = np.zeros((num, num))
+    for x in range(num):
+        for y in range(num):
+            rdm[x,y] = np.count_nonzero(edges[x] & edges[y])
+            if x == y:
+                rdm[x,y] = 0
+    # Normalize values
+    rdm = rdm / rdm.max()
+    return edges, rdm
+
+def categorical_model(dataset, folder):
     gnb = GaussianNB()
-    activations = loadmat("Feature_Extract/rdms/92images_rdms/vgg/pearson/fc8/submit_fmri.mat")
+    activations = loadmat("vgg_fc8_fmri.mat")
     print(type(activations))
 
     # y_pred = gnb.fit(iris.data, iris.target).predict(iris.data)
 
-def load_data(dataset_name, scale=100):
+def load_data(folder, scale=100):
     # Define rescale transform to (default) 100x100 pixels
     data_transform = transforms.Compose([
         transforms.Resize((scale, scale)),
@@ -73,35 +86,35 @@ def load_data(dataset_name, scale=100):
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                             std=[0.229, 0.224, 0.225])
     ])
-
-    dataset = datasets.ImageFolder(root='Training_Data/' + dataset_name,
+    dataset = datasets.ImageFolder(root='Training_Data/' + folder,
                                            transform=data_transform)
-
-    return dataset, data_loader
-
-
+    return dataset
 
 if __name__ == "__main__":
+    # Select the datasets here
+    training_folder = '92_Image_Set_Labeled'
     # load the training and test data
-    train_dataset = load_data('92_Image_Set_Labeled')
-    val_dataset = load_data('92_Image_Set')
+    train_dataset = load_data(training_folder)
+    # val_dataset = load_data('92_Image_Set')
+
+    # Perceptual model
+    # print("Running perceptual model on: ", train_folder)
+    # p_edges, p_rdm = perceptual_model(train_dataset, training_folder, 1)
+    # plt.figure()
+    # plt.pcolor(p_rdm)
+    # plt.colorbar()
+    # plt.show()
+
+    # Categorical model
     # Show the classes and indexes
     print(train_dataset.class_to_idx)
-    # print(train_dataset.imgs)
-
-    # Load training data using dataloader
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32,
-                                                num_workers=4, shuffle=True))
-    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=32,
-                                                num_workers=4, shuffle=True))
+    categorical_model(train_dataset, training_folder)
+    # val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=32,
+                                                # num_workers=4, shuffle=True)
 
     # Get pretrained model using torchvision.models as models library
-    model = models.densenet161(pretrained=True)
-    # Turn off training for their parameters
-    for param in model.parameters():
-        param.requires_grad = False
-    # Edge detection
-    # print("Running edge detection on: ", train_dataset)
-    # edge_detect(train_dataset, iter(train_loader), 1)
+    # model = models.densenet161(pretrained=True)
 
-    # GNB()
+    # Turn off training for their parameters
+    # for param in model.parameters():
+    #     param.requires_grad = False
